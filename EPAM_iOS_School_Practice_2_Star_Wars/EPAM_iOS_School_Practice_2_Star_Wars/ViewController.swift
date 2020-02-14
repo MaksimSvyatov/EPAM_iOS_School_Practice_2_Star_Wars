de2e8f5a3834ebccca2410660ebb80379d26c7ff
Maksim Svyatov <masvjatov@gmail.com>
2020-02-14 10:45:47 +0300

    Create project

---
+++ EPAM_iOS_School_Practice_2_Star_Wars/EPAM_iOS_School_Practice_2_Star_Wars/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EPAM_iOS_School_Practice_2_Star_Wars
+//
+//  Created by Maxim on 14.02.2020.
+//  Copyright © 2020 Maksim Svyatov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
