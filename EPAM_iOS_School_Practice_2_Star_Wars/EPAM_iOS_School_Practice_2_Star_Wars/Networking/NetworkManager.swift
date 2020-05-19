9e3f7f3ad3808e510e15cc34af229eab01afaf40
Maksim <masvjatov@gmail.com>
2020-05-19 12:26:57 +0300

    Fix

---
+++ EPAM_iOS_School_Practice_2_Star_Wars/EPAM_iOS_School_Practice_2_Star_Wars/Networking/NetworkManager.swift
@@ -0,0 +1,36 @@
+//
+//  NetworkManager.swift
+//  EPAM_iOS_School_Practice_2_Star_Wars
+//
+//  Created by Maxim on 27.02.2020.
+//  Copyright © 2020 Maksim Svyatov. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+protocol SearchServiceProtocol {
+    func searchPeople(for name: String, completion: @escaping (Result<People?, Error>) -> ())
+}
+
+class SearchService: SearchServiceProtocol {
+    
+    func searchPeople(for name: String, completion: @escaping (Result<People?, Error>) -> ()) {
+        let url = "http://swapi.dev/api/people"
+        let parameters: [String: String] = ["search": name]
+        AF.request(url, parameters: parameters)
+            .validate()
+            .responseDecodable(of: People.self) { response in
+                if let error = response.error {
+                    completion(.failure(error))
+                    return
+                }
+                
+                guard let people = response.value else {
+                    return }
+                
+                completion(.success(people))
+                
+        }
+    }
+}
